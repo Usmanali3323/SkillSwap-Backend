eb29da93c9014fa7d8113b311be7a0caa92b0337
Usman Ali <108189887+Usmanali3323@users.noreply.github.com>
2025-07-15 21:26:36 +0500

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,10 @@ const app = express();
 // const allowedOrigins = ['http://localhost:5173']; // Frontend URL
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://skillswap-a5313.web.app',
+  credentials: true,
+}));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
