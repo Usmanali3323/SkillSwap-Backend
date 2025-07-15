197c361328974f6ffd2667b0ceaf9ed20a5cb4b4
Usman Ali <108189887+Usmanali3323@users.noreply.github.com>
2025-07-15 20:20:06 +0500

    Update index.js

--- index.js
+++ index.js
@@ -18,13 +18,10 @@ dotenv.config({ path: "./.env" });
 const app = express();
 
 
-const allowedOrigins = ['http://localhost:5173']; // Frontend URL
+// const allowedOrigins = ['http://localhost:5173']; // Frontend URL
 
 // Middleware
-app.use(cors({
-  origin: allowedOrigins,
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
